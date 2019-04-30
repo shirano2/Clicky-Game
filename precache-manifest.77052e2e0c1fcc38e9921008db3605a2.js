6f83e689e31a534fa51c42432591fa6b05ee4b09
shirano2 <shirano2@naver.com>
2019-04-30 00:04:43 -0500

    Updates

---
+++ precache-manifest.77052e2e0c1fcc38e9921008db3605a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9f992aea74946a504dab305eef4e50",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "29e12fbe96eb11ed4861",
+    "url": "/clicky-game/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicky-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "29e12fbe96eb11ed4861",
+    "url": "/clicky-game/static/js/main.811fc60f.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
